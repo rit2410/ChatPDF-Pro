f70e50999bd026aad1210656569c0761946cb0b8
Ritika Pandey <ritikap2402@gmail.com>
2025-10-18 13:17:58 +0530

    Fix import statement for RecursiveCharacterTextSplitter

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 import tempfile
 from langchain_groq import ChatGroq
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter 
+from langchain_text_splitters import RecursiveCharacterTextSplitter  # CHANGED
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 from langchain.chains import create_retrieval_chain
